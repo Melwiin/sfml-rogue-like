1a6da2b5cfdc33bc35a71dc7c04c9280255b4a72
Eddie Dover <ed@eddiedover.dev>
2023-06-21 18:59:23 -0700

    Updated EntityManager to auto name elements. Added ability for EntityManager to find elements by name.

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -1,13 +1,37 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <string>
+
+enum class EntityEnumType {
+    Player,
+    Enemy,
+    Projectile,
+    None
+};
 
 class Entity {
 protected:
     sf::RectangleShape shape;
+    std::string name;
+    EntityEnumType type;
 public:
     virtual void update(float deltaTime) = 0;
     virtual void render(sf::RenderWindow& window) const = 0;
+    const std::string & getName() const {
+         return this->name;
+    }
+    void setName(const std::string & name) {
+        this->name = name;
+    }
+
+    const EntityEnumType & getType() const {
+        return this->type;
+    }
+
+    void setType(const EntityEnumType & type) {
+        this->type = type;
+    }
 
     sf::Vector2f getPosition() const {
         return shape.getPosition();
--- include/EntityManager.hpp
+++ include/EntityManager.hpp
@@ -3,22 +3,64 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <memory>
+#include <iostream>
+#include <string>
 
 #include "Entity.hpp"
+
 
 class EntityManager {
 private:
     std::vector<std::shared_ptr<Entity>> entities;
 
+    int countByType(EntityEnumType type) {
+        int count = 0;
+        for (const auto& entity : entities) {
+            if (entity->getType() == type) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 public:
     template <typename EntityType, typename... Args>
-    std::shared_ptr<EntityType> createEntity(Args&&... args) {
+    std::shared_ptr<EntityType> createEntity(EntityEnumType type, Args&&... args) {
         static_assert(std::is_base_of<Entity, EntityType>::value, "EntityType must be derived from Entity");
 
         std::shared_ptr<EntityType> entity = nullptr;
         entity = std::make_shared<EntityType>(std::forward<Args>(args)...);
+        
+        std::string typestring = "";
+        switch (type) {
+        case EntityEnumType::Player:
+            typestring = "Player";
+            break;
+        case EntityEnumType::Enemy:
+            typestring = "Enemy";
+            break;
+        case EntityEnumType::Projectile:
+            typestring = "Projectile";
+            break;
+        default:
+            typestring = "None";
+            break;
+        }
+        std::string name = typestring + std::to_string(countByType(type));
+        
+        entity->setType(type);
+        entity->setName(name);
         entities.push_back(entity);
         return entity;
+    }
+
+    std::shared_ptr<Entity> getByName(std::string name) {
+        for (const auto& entity : entities) {
+            if (entity->getName() == name) {
+                return entity;
+            }
+        }
+        return nullptr;
     }
 
     void updateEntities(float deltaTime) {

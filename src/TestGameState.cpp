bb2bb568369c47b42a2ff242ece7b5b31507a522
Eddie Dover <ed@eddiedover.dev>
2023-06-21 18:59:54 -0700

    Moved firing code into TestGameState

--- src/TestGameState.cpp
+++ src/TestGameState.cpp
@@ -1,10 +1,12 @@
 #include "TestGameState.hpp"
-
+#include "Projectile.hpp"
 TestGameState::TestGameState(GameDataRef data) : m_data(data) {}
 
 void TestGameState::init() {
 
-    m_player = m_entityManager.createEntity<Player>(&m_entityManager);
+    spacebarPressed = false;
+
+    m_player = m_entityManager.createEntity<Player>(EntityEnumType::Player);
 
     m_data->camera.setSize(m_data->window.getSize().x, m_data->window.getSize().y);
 
@@ -19,7 +21,21 @@ void TestGameState::handleEvents() {
     if(sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
         // get position of mouse relative to window and view
         const sf::Vector2f mousePosition = m_data->window.mapPixelToCoords(sf::Mouse::getPosition(m_data->window), m_data->camera);
-        m_enemies.push_back(m_entityManager.createEntity<Enemy>(mousePosition.x, mousePosition.y));
+        m_enemies.push_back(m_entityManager.createEntity<Enemy>(EntityEnumType::Enemy, mousePosition.x, mousePosition.y));
+    }
+
+    if (!spacebarPressed && sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+    {
+        spacebarPressed = true; // Spacebar pressed, set the flag
+        sf::Vector2f projectilePosition = m_entityManager.getByName("Player0")->getPosition() - sf::Vector2f(20, 20);
+        sf::Vector2f projectileDirection(0.0f, -1.0f);
+        float projectileSpeed = 500.0f; 
+
+        auto entity = m_entityManager.createEntity<Projectile>(EntityEnumType::Projectile,projectilePosition, projectileDirection, projectileSpeed);
+    }
+    else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+    {
+        spacebarPressed = false; // Spacebar released, reset the flag
     }
 }
 
